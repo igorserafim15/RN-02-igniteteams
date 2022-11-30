cc2737387001335cf017306a2afa41be2fbf7cb5
Igor Serafim <igorserafim2015@hotmail.com>
2022-11-30 11:57:40 -0300

    first components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
+import { Container } from './styles'
+
+export const Groups = () => {
+  return (
+    <Container>
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+    </Container>
+  )
+}
